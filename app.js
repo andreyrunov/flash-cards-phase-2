5ad1fd53cb4e18f1c7842ab1953119450e0c20d6
Andrey Runov <elefant86@bk.ru>
2022-05-20 12:47:34 +0300

    server ready

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const hbs = require('hbs');
+const path = require('path');
+const { Themes, Posts } = require('./db/models');
+const PORT = process.env.DB_PORT ?? 3000;
+const app = express();
+app.set('view engine', 'hbs');
+hbs.registerPartials(`${__dirname}/views/partials`);
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(process.env.PWD, 'public')));
 
+
+app.listen(PORT, () => {
+console.log('Server start on port', PORT);
+});
